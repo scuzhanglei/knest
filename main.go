dce59f8c7ad5c3a8d2d191914ec888fade4872f2
scuzhanglei <greatzhanglei@gmail.com>
2022-10-10 11:45:15 +0800

    feat: Add knest version command (#1)

--- main.go
+++ main.go
@@ -22,6 +22,8 @@ const (
 	VirtinkProviderVersion = "v0.4.0"
 )
 
+var version string
+
 func main() {
 	var (
 		targetNamespace                          = "default"
@@ -270,6 +272,15 @@ func main() {
 		},
 	}
 
+	cmdVersion := &cobra.Command{
+		Use:   "version",
+		Short: "Print knest version.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Printf("knest version: %#v, virtink version: %#v, cluster-api-provider-virtink version: %#v\n", version, VirtinkVersion, VirtinkProviderVersion)
+			return nil
+		},
+	}
+
 	rootCmd := &cobra.Command{
 		Use: "knest",
 	}
@@ -278,6 +289,7 @@ func main() {
 	rootCmd.AddCommand(cmdDelete)
 	rootCmd.AddCommand(cmdList)
 	rootCmd.AddCommand(cmdScale)
+	rootCmd.AddCommand(cmdVersion)
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
